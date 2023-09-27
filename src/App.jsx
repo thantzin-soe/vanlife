f970d3cc00e37506b66d3a6a34566de1d1cbcd61
thantzin-soe <tzsoe100@gmail.com>
2023-09-27 19:53:45 +0630

    add styles to HomePage and AboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AboutPage from './pages/AboutPage.jsx'
-import HomePage from './pages/HomePage.jsx'
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AboutPage from "./pages/AboutPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/about" element={<AboutPage/>}></Route>
-      </Routes>
-    </BrowserRouter>
+        <header>
+          <Link className="site-logo" to="/">
+            #VANLIFE
+          </Link>
+          <nav>
+            <Link to="/about">About</Link>
+            <Link to="/">Vans</Link>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
